f1a590cec4a45c99a6f77f4a20d15e5e2722ed22
brx <vsavic89@gmail.com>
2021-07-15 09:42:14 +0200

    controller paginate, blade pagination

--- resources/views/gigatron/products/index.blade.php
+++ resources/views/gigatron/products/index.blade.php
@@ -1,13 +1,14 @@
 @extends('master')
 @section('content')        
     @if(count($gigatronProducts) > 0)  
-        <table class="table table-responsive border">
-            <caption>List of products</caption>
+        <h1>List of products</h1>
+        <table class="table table-responsive border">            
             <tr>
                 <th>Image</th>
                 <th>Product Id</th>
                 <th>Product Name</th>            
                 <th>EAN</th>
+                <th>Brand</th>
                 <th>Price</th>
             </tr>
             @foreach($gigatronProducts as $product)
@@ -16,10 +17,12 @@
                     <td>{{ $product->id }}</td>
                     <td>{{ $product->name }}</td>   
                     <td>{{ $product->ean }}</td>
+                    <td>{{ $product->brand->name }}</td>
                     <td class="price">{{ $product->price }}</td>                 
                 </tr>
             @endforeach
         </table>
+        {{ $gigatronProducts->links() }}
     @else
         <h1>No records to show.</h1>
     @endif    
